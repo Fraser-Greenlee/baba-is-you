d8dfa0f7d1f259a49684631989e92358ca7bdc53
Fraser Greenlee <fraser.greenlee@skyscanner.net>
2020-11-30 12:41:24 +0000

    initial assets

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+
+
+App()
