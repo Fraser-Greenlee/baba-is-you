8572f3fc958a3be6eedab50fc5f78d18b51f002b
Fraser Greenlee <fraser.greenlee@skyscanner.net>
2020-12-24 09:42:25 +0000

    update parse paths

--- config.py
+++ config.py
@@ -8,33 +8,30 @@ MOVE_ATTEMPTS = 10
 END_SPRITE_INDEX = 6
 # Basic parse tree but should work well enough
 L0_PARSE_PATHS = {
-    NounTile: {
-        IsTile: {
-            NounTile: IsNoun,
-            PropertyTile: {
-                None: IsProperty,
-                AndTile: {
-                    PropertyTile: {
-                        None: IsProperty,
-                        AndTile: {PropertyTile: IsProperty},
-                    },
-                }
+    IsTile: {
+        NounTile: IsNoun,
+        PropertyTile: {
+            None: IsProperty,
+            AndTile: {
+                PropertyTile: {
+                    None: IsProperty,
+                    AndTile: {PropertyTile: IsProperty},
+                },
             }
-        },
-        HasTile: {NounTile: Has},
-        MakeTile: {NounTile: Has},
-    }
-}
-L1_PARSE_PATHS = {
-    OnTile: {NounTile: L0_PARSE_PATHS},
-    **L0_PARSE_PATHS
+        }
+    },
+    HasTile: {NounTile: Has},
+    MakeTile: {NounTile: Has},
 }
 PARSE_PATHS = {
     NounTile: {
         AndTile: {
-            **L1_PARSE_PATHS
+            NounTile: L0_PARSE_PATHS
         },
-        **L1_PARSE_PATHS
+        OnTile: {
+            NounTile: L0_PARSE_PATHS
+        },
+        **L0_PARSE_PATHS
     }
 }
 
